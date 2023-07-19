2950da91ebf248c739f3d43cd11233e5f2ab90e1
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-19 13:50:13 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
 import Hero from './components/Hero';
 import Table from './components/table';
 import Tabl from './components/tabl';
+import './components/change.css'
 import Sport from './components/sport';
 import React from 'react';
 import Head from 'next/head'
 
-const Home = () => {
+export default function Home() {
+
+  useEffect(() => {
+    document.querySelector('.animated').classList.add('fade');
+
+    return () => {
+      document.querySelector('.animated').classList.remove('fade');
+    }
+  }, []);
+
   return (
-    <>
-<Head>
+    <div className="animated">
+       <Head>
         <title>雙龍體育</title>
         <meta name="description" content="雙龍體育網站 - 專業體育賽事！ 提供最新的雙龍體育資訊、TV、運動比賽報導，讓您輕鬆了解雙龍體育。歡迎加入雙龍運動論壇，一起與大家溝通！" />
       </Head>
@@ -20,8 +30,6 @@ const Home = () => {
         <Table />
         <Tabl />
       </div>
-    </>
-  );
+    </div>
+  )
 }
-
-export default Home;
