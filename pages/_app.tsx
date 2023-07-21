066d60effaf1307838583c75db9a68ebdc58f063
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-21 14:03:44 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       
-    <navbar />
+    <Navbar />
       <div />
 <Component {...pageProps} />
    </div>
