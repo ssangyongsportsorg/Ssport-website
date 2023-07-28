31e516049d1373dd5d521a82d3f0eb179fc0095a
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-28 20:14:23 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Head from './components/head'
 import Footer from './components/footer'
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    </div>
       
     <Head />
             <SessionProvider session={session}>
