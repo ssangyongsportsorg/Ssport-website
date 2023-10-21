571da5c5a963839e3c167e9b4ac177f08d59a0a9
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-21 19:12:56 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import { Metadata } from 'next'
 import type { AppProps } from 'next/app'
 import Head from './components/head'
 import Footer from './components/footer'
