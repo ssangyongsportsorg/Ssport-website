00b5c1514a9fdbd9043578ccbab44b9b7f12f998
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-21 10:36:06 +0800

    Update page.tsx

--- app/docs/[...slug]/page.tsx
+++ app/docs/[...slug]/page.tsx
@@ -1,4 +1,4 @@
-import { getPage, getPageUrl, tree } from '@/pages/source'
+import { getPage, getPageUrl, tree } from '@/app/source'
 import { allDocs } from 'contentlayer/generated'
 import type { Metadata } from 'next'
 import { MDXContent } from 'next-docs-ui/mdx'
