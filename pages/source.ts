f6a25e21b575cbaf2f3ad412a72988f17af8808a
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-21 10:10:41 +0800

    Update source.ts

--- pages/source.ts
+++ pages/source.ts
@@ -1,27 +1,7 @@
 import { allDocs, allMeta } from 'contentlayer/generated'
-import {
-  buildPageTree,
-  createUtils,
-  loadContext
-} from 'next-docs-zeta/contentlayer'
-import type { PageTree } from 'next-docs-zeta/server'
+import { createContentlayer } from 'next-docs-zeta/contentlayer'
 
-const ctx = loadContext(allMeta, allDocs)
-
-const uiTree = buildPageTree(ctx, {
-  root: 'docs/ui'
-})
-
-const headlessTree = buildPageTree(ctx, {
-  root: 'docs/headless'
-})
-
-export function getTree(mode: 'ui' | 'headless' | string): PageTree {
-  if (mode === 'ui') {
-    return uiTree
-  }
-
-  return headlessTree
-}
-
-export const { getPage, getPageUrl } = createUtils(ctx)
+export const { tree, getPage, getPageUrl } = createContentlayer(
+  allMeta,
+  allDocs
+)
