5d26229cec9563c5e5b504a612e33dec30686900
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-21 19:20:09 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,14 @@
 import '@/styles/globals.css'
-import { Metadata } from 'next'
-import type { AppProps } from 'next/app'
-import Head from './components/head'
-import Footer from './components/footer'
-import { SessionProvider } from 'next-auth/react'
-import Script from 'next/script';
-export const metadata: Metadata = {
-  title: {
-    template: "Yeecord - %s",
-    default: "Yeecord - 萬中選一的 Discord 機器人",
-  },
-  description:
-    "YEE式機器龍的指令及使用教學，透過簡單的一鍵式指令以及中文介面的音樂功能快速建立好和朋友玩耍的優質空間",
-  };
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="zh-Hant-TW" className={noto.className}>
-      <body>
-        <SessionProvider session={session}>
-        <div>
-  <Script src="/chat.js" />
-        <Head />
-            {children}
-            <Footer />
-        </div>
-    </SessionProvider>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
-  );
+  )
 }
