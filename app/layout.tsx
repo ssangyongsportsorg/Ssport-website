26980eb86d4178498bf2d0f383b5e2c20669be2a
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-21 18:42:04 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,7 @@
+export const metadata: Metadata = {
+  title: {
+    template: "Yeecord - %s",
+    default: "Yeecord - 萬中選一的 Discord 機器人",
+  },
+  description:
+    "YEE式機器龍的指令及使用教學，透過簡單的一鍵式指令以及中文介面的音樂功能快速建立好和朋友玩耍的優質空間",
