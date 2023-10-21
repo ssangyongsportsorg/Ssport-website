7dc1aea071474c919dc108c154453da147580753
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-21 18:58:02 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from './components/head'
+import Footer from './components/footer'
+import { SessionProvider } from 'next-auth/react'
+import Script from 'next/script';
 export const metadata: Metadata = {
   title: {
     template: "Yeecord - %s",
@@ -5,3 +11,20 @@ export const metadata: Metadata = {
   },
   description:
     "YEE式機器龍的指令及使用教學，透過簡單的一鍵式指令以及中文介面的音樂功能快速建立好和朋友玩耍的優質空間",
+  };
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="zh-Hant-TW" className={noto.className}>
+      <body>
+        <SessionProvider session={session}>
+        <div>
+  <Script src="/chat.js" />
+        <Head />
+            {children}
+            <Footer />
+        </div>
+    </SessionProvider>
+      </body>
+    </html>
+  );
+}
