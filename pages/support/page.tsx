1fb599f4bdceb3f31d6833d35a1fdcd2204819fd
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-21 18:29:13 +0800

    Create page.tsx

---
+++ pages/support/page.tsx
@@ -0,0 +1,26 @@
+import Supportchoice from '../components/supportchoice';
+import Supporthero from '../components/supporthero';
+import Ticket from '../components/ticket';
+
+import Head from 'next/head'
+
+const support = () => {
+  return (
+       <>
+<Head>
+        <title>雙龍體育幫助</title>
+        <meta name="description" content="歡迎來到雙龍體育幫助中心,任何問題立刻聯繫" />
+      </Head>
+      
+      <div>
+
+               <Supporthero />
+
+        <Supportchoice />
+                <Ticket />
+      </div>
+    </>
+      );
+}
+
+export default support;
