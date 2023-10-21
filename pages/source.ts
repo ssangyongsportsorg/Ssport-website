fac10a2134febb1a96f18e25a307e538c8d90864
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-21 10:12:39 +0800

    Update source.ts

--- pages/source.ts
+++ pages/source.ts
@@ -1,7 +1,7 @@
 import { allDocs, allMeta } from 'contentlayer/generated'
 import { createContentlayer } from 'next-docs-zeta/contentlayer'
 
-export const { tree, getPage, getPageUrl } = createContentlayer(
+export const { getPage, getPageUrl, tree } = createContentlayer(
   allMeta,
   allDocs
 )
