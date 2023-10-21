e6016bf84ea548dca01f5642aaa07d81ebefbff9
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-21 14:16:52 +0800

    Add files via upload

---
+++ app/components/supporthero.tsx
@@ -0,0 +1,19 @@
+import styles from './Button.module.css';
+
+
+function supporthero() {
+  return (
+<section
+  className="bg-white dark:bg-gray-900"
+>
+  <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+    <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+      需要 <span className={styles.abc}>雙龍體育</span>的幫助嗎?
+    </h2>
+  </div>
+</section>
+
+  )
+}
+
+export default supporthero;
