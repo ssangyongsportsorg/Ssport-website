338f18938e43f77bac058bfa50c4e8cfbc705e9e
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-22 00:41:08 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
 import '@/styles/globals.css'
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from './components/head'
 import Footer from './components/footer'
 import { SessionProvider } from 'next-auth/react'
 import Script from 'next/script';
-
 export default function RootLayout({
+  
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
   children,
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="zh-tw">
-      <SessionProvider session={session}>
+      
         <div>
-          <Script src="/chat.js" />
-          <Head />
-          <body>
-            {children}
-            {Component && <Component {...pageProps} />}
-          </body>
+  <Script src="/chat.js" />
+        <Head />
+      <body>{children}</body>
           <Footer />
         </div>
-      </SessionProvider>
+  
     </html>
   )
-}
+} 
