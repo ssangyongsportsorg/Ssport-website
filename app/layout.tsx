431c8f107480e29c05c0960336dc2255fa6968e8
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-22 00:36:03 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,6 @@ import Script from 'next/script';
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
-  children,
-}: {
-  children: React.ReactNode
-}) {
   Component, 
   pageProps: { session, ...pageProps },
 }: AppProps) {
