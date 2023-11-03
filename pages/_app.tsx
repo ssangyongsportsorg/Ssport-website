9cda658edab913b73a668d11955d1fa8645ea05e
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-04 00:42:43 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Head from './components/head'
 import Footer from './components/footer'
 import { SessionProvider } from 'next-auth/react'
 import Script from 'next/script';
+import { Bubble } from "@typebot.io/nextjs";
 
 export default function App({
   Component, 
@@ -13,7 +14,12 @@ export default function App({
     <div>
       
         <div className="bg-gradient h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
-        <SessionProvider session={session}>
+      
+<Bubble
+      typebot="customer-support-q6zv3pv"
+     apiHost="https://ssportbotview.vercel.app"
+      theme={{ button: { backgroundColor: "#0042DA" } }}
+    />        <SessionProvider session={session}>
           <Head />
           <Component {...pageProps} />
           <Footer />
