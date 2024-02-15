944bde17b9e7f5cf901905c26c212ae30588ca1b
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-02-15 13:31:44 +0800

    Update login.tsx

--- pages/components/login.tsx
+++ pages/components/login.tsx
@@ -1,9 +1,14 @@
+
 'use client';
 import React from 'react';
 import { Button } from "flowbite-react";
 import { Dropdown, Avatar } from 'flowbite-react';
 import Link from 'next/link';
-import { LoginLink, LogoutLink, RegisterLink, getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 export default function Login() {
   const Navbar = () => {
