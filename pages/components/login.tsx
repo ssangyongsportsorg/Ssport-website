7a6acc0ac4d21e58da70fb4251635c612b71724f
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-02-15 13:18:07 +0800

    Update login.tsx

--- pages/components/login.tsx
+++ pages/components/login.tsx
@@ -1,47 +1,48 @@
 'use client';
-import {
-  LoginLink,
-  LogoutLink,
-  RegisterLink,
-  getKindeServerSession,
-} from '@kinde-oss/kinde-auth-nextjs/server'
 import React from 'react';
 import { Button } from "flowbite-react";
-import { Dropdown, Navbar, Avatar } from 'flowbite-react';
-import Link from 'next/link'
+import { Dropdown, Avatar } from 'flowbite-react';
+import Link from 'next/link';
+import { LoginLink, LogoutLink, RegisterLink, getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+
 export default function Login() {
-const Navbar = () => {
-  const { getUser } = getKindeServerSession()
-  const user = getUser()
-return (
-<>
-   {user?.picture ? (
-<Dropdown inline label={<Avatar alt="User settings" img={`${user?.picture}`} rounded/>} > 
-     ) : (
-  <div>
-   {user?.given_name?.[0]}
-       {user?.email?.[0]}
-  
-  <div/>
-     )}
-  <Dropdown.Header> <span className="block text-sm">${user.family_name}  </span> <span className="block truncate text-sm font-medium"> {user.email} </span> </Dropdown.Header> 
-  <div> <Link href="/watch-tv">觀看TV</Link></div> <div> <Link href="#">購物</Link> </div> <div> <Link href="/support">幫助</Link> </div> <Dropdown.Divider /> <div>  )}
- <LogoutLink className="text-subtle">登出</LogoutLink> </div> </Dropdown>
-</>
-)
-}
-return (
-<>
-<button
-        className="break-keep rounded-md px-5 py-1 font-bold"
-      >
- <RegisterLink
-                  className={buttonVariants({
-                    variant: 'ghost',
-                    size: 'sm',
-                  })}>
-                 註冊
-                </RegisterLink></button>
-</>
-)
+  const Navbar = () => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+
+    return (
+      <>
+        {user?.picture ? (
+          <Dropdown inline label={<Avatar alt="User settings" img={`${user?.picture}`} rounded />} >
+            <Dropdown.Header>
+              <span className="block text-sm">${user.family_name}</span>
+              <span className="block truncate text-sm font-medium">{user.email}</span>
+            </Dropdown.Header>
+            <div> <Link href="/watch-tv">觀看TV</Link></div> 
+            <div> <Link href="#">購物</Link> </div> 
+            <div> <Link href="/support">幫助</Link> </div> 
+            <Dropdown.Divider />
+            <div>
+              <LogoutLink className="text-subtle">登出</LogoutLink>
+            </div>
+          </Dropdown>
+        ) : (
+          <div>
+            {user?.given_name?.[0]}
+            {user?.email?.[0]}
+          </div>
+        )}
+      </>
+    );
+  };
+
+  return (
+    <>
+      <button className="break-keep rounded-md px-5 py-1 font-bold">
+        <RegisterLink className={buttonVariants({ variant: 'ghost', size: 'sm' })}>
+          註冊
+        </RegisterLink>
+      </button>
+    </>
+  );
 }
