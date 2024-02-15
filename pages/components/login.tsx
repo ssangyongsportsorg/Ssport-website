ce010ab904e52617f01cf4aeb8a97c19dc77665f
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-02-15 13:44:34 +0800

    Update login.tsx

--- pages/components/login.tsx
+++ pages/components/login.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 import React from 'react';
 import { Button } from "flowbite-react";
@@ -10,50 +9,48 @@ import {
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
 export default function Login() {
-  const Navbar = () => {
-    const { getUser } = getKindeServerSession();
-    const user = getUser();
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const user = getUser();
 
-    return (
-      <>
-                <nav className="nav container">
-            <h1 className="text-display-3">KindeAuth</h1>
-            <div>
-              {!(await isAuthenticated()) ? (
-                <>
-                  <LoginLink className="btn btn-ghost sign-in-btn">
-                    Sign in
-                  </LoginLink>
-                  <RegisterLink className="btn btn-dark">Sign up</RegisterLink>
-                </>
+  return (
+    <>
+      <nav className="nav container">
+        <h1 className="text-display-3">KindeAuth</h1>
+        <div>
+          {!isAuthenticated() ? (
+            <>
+              <LoginLink className="btn btn-ghost sign-in-btn">
+                Sign in
+              </LoginLink>
+              <RegisterLink className="btn btn-dark">Sign up</RegisterLink>
+            </>
+          ) : (
+            <div className="profile-blob">
+              {user?.picture ? (
+                <img
+                  className="avatar"
+                  src={user?.picture}
+                  alt="user profile avatar"
+                  referrerPolicy="no-referrer"
+                />
               ) : (
-                <div className="profile-blob">
-                  {user?.picture ? (
-                    <img
-                      className="avatar"
-                      src={user?.picture}
-                      alt="user profile avatar"
-                      referrerPolicy="no-referrer"
-                    />
-                  ) : (
-                    <div className="avatar">
-                      {user?.given_name?.[0]}
-                      {user?.family_name?.[0]}
-                    </div>
-                  )}
-                  <div>
-                    <p className="text-heading-2">
-                      {user?.given_name} {user?.family_name}
-                    </p>
-
-                    <LogoutLink className="text-subtle">Log out</LogoutLink>
-                  </div>
+                <div className="avatar">
+                  {user?.given_name?.[0]}
+                  {user?.family_name?.[0]}
                 </div>
               )}
+              <div>
+                <p className="text-heading-2">
+                  {user?.given_name} {user?.family_name}
+                </p>
+                <LogoutLink className="text-subtle">Log out</LogoutLink>
+              </div>
             </div>
-          </nav>
-      <>
-    );
-  };
-
+          )}
+        </div>
+      </nav>
+    </>
+  );
+}
