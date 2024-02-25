61e6e0233a17506851cf3b206cb665b0df07143b
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2024-02-25 18:07:17 +0800

    Update tabl.tsx

--- pages/components/tabl.tsx
+++ pages/components/tabl.tsx
@@ -18,7 +18,7 @@ function Tabl() {
             href="https://sba.ssangyongsports.eu.org/"
             className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900"
           >
-           更多資訊
+           訪問官網
             <svg
               className="ml-2 -mr-1 w-5 h-5"
               fill="currentColor"
