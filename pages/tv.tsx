01b8304b3025a19c4197697a46a5317ac790937b
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-03-17 16:14:44 +0800

    Update tv.tsx

--- pages/tv.tsx
+++ pages/tv.tsx
@@ -1,25 +1,27 @@
-import Tvhero from './components/Tvhero';
-import Price from './components/price';
-import Feature from './components/feature';
-import React from 'react';
-import Head from 'next/head'
-import Chatbot from './components/chatbot';
-const Home = () => {
+import CTA from "@/components/ui/CTA";
+import FAQs from "@/components/ui/FAQs";
+import Features from "@/components/ui/Features";
+import Hero from "@/components/ui/Hero";
+import Pricing from "@/components/ui/Pricing";
+import Testimonial from "@/components/ui/Testimonial";
+import VisualFeatures from "@/components/ui/VisualFeatures";
+
+export default function TV() {
   return (
     <>
-<Head>
+      <Hero />
+      <VisualFeatures />
+      <Features />
+      <CTA />
+      <Testimonial />
+      <Pricing />
+      <FAQs />
+      <Head>
         <title>雙龍體育TV</title>
         <meta name="description" content="雙龍體育TV直播比賽，快速，安全立刻購買！ 註冊登入" />
       </Head>
-      <div>
-        <Tvhero />
-    
-        <Feature />
-        <Price />
-       
-      </div>
     </>
   );
 }
 
-export default Home;
+
