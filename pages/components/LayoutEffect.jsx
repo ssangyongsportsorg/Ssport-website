4c4663033bd9a1bffb353aac2e205e42c974258d
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-03-17 17:59:37 +0800

    Update LayoutEffect.jsx

--- pages/components/LayoutEffect.jsx
+++ pages/components/LayoutEffect.jsx
@@ -1,18 +1,7 @@
-import { useInView } from "framer-motion"
-import { cloneElement, useRef } from "react";
-
-const LayoutEffect = ({ children,
-    className,
-    isInviewState: { trueState = "", falseState = "" }
-}) => {
-
-    const ref = useRef(null)
-    const isInView = useInView(ref, { once: true })
-
-    return cloneElement(children, {
-        ref,
-        className: `${children.props.className || ""} ${className || ""} ${isInView ? trueState : falseState}`
-    })
+function LayoutEffect() {
+return (
+    <div><div/>
+)
 }
 
-export default LayoutEffect
+export default LayoutEffect;
