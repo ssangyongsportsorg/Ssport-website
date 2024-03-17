223051d2ed7f8e97bc9ea24ecaf861650b10d726
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-03-17 17:09:26 +0800

    Update Hero.jsx

--- pages/components/Hero/Hero.jsx
+++ pages/components/Hero/Hero.jsx
@@ -1,8 +1,8 @@
 import GradientWrapper from "@/components/GradientWrapper"
 import Image from "next/image"
-import NavLink from "../NavLink"
+import Link from 'next/link'
 import HeroImg from "@/public/images/hero.svg"
-import LayoutEffect from "@/components/LayoutEffect"
+import LayoutEffect from "../LayoutEffect"
 
 const Hero = () => (
     <section>
@@ -26,7 +26,7 @@ const Hero = () => (
                             Gain control of your business's growth with Mailgo's comprehensive marketing, automation, and email marketing platform.
                         </p>
                         <div className="flex justify-center font-medium text-sm">
-                            <NavLink
+                            <Link
                                 href="/#pricing"
                                 className="flex items-center text-white bg-purple-600 hover:bg-purple-500 active:bg-purple-700 "
                             >
@@ -34,7 +34,7 @@ const Hero = () => (
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                     <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
                                 </svg>
-                            </NavLink>
+                            </Link>
                         </div>
                     </div>
                     <GradientWrapper className="mt-16 sm:mt-28" wrapperClassName="max-w-3xl h-[250px] top-12 inset-0 sm:h-[300px] lg:h-[650px]">
